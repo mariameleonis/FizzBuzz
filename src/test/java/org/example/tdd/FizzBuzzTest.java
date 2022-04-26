964ed1fa6e084b69b5e9baf429ba0371c41d259a
Maria <maria@MacBook-Pro-Maria.local>
2022-04-27 01:28:54 +0600

    test: add test scenarios

--- src/test/java/org/example/tdd/FizzBuzzTest.java
+++ src/test/java/org/example/tdd/FizzBuzzTest.java
@@ -2,15 +2,49 @@ package org.example.tdd;
 
 import org.junit.jupiter.api.*;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class FizzBuzzTest {
 
-    @DisplayName("Divisible By Three")
+    @DisplayName("Divisible by Three")
     @Test
     @Order(1)
     void testForDivisibleByThree() {
-        fail("fail");
+
+        String expected = "Fizz";
+
+        assertEquals(expected, FizzBuzz.compute(3), "Should return Fizz");
+    }
+
+    @DisplayName("Divisible by Five")
+    @Test
+    @Order(2)
+    void testForDivisibleByFive() {
+
+        String expected = "Buzz";
+
+        assertEquals(expected, FizzBuzz.compute(5), "Should return Buzz");
+    }
+
+    @DisplayName("Divisible by Three and Five")
+    @Test
+    @Order(3)
+    void testForDivisibleByThreeAndFive() {
+
+        String expected = "FizzBuzz";
+
+        assertEquals(expected, FizzBuzz.compute(15), "Should return FizzBuzz");
+    }
+
+    @DisplayName("Not Divisible by Three or Five")
+    @Test
+    @Order(4)
+    void testForNotDivisibleByThreeOrFive() {
+
+        String expected = "7";
+
+        assertEquals(expected, FizzBuzz.compute(7), "Should return 7");
     }
 }
