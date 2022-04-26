c9dfa5660cf1d2231209fc3edbdf31058956316f
Mariya <russakova.m@gmail.com>
2022-04-27 02:19:47 +0600

    test: add parameterized test

--- src/test/java/org/example/tdd/FizzBuzzTest.java
+++ src/test/java/org/example/tdd/FizzBuzzTest.java
@@ -1,6 +1,8 @@
 package org.example.tdd;
 
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
@@ -46,5 +48,14 @@ class FizzBuzzTest {
         String expected = "7";
 
         assertEquals(expected, FizzBuzz.compute(7), "Should return 7");
+    }
+
+    @DisplayName("Testing with data file")
+    @ParameterizedTest(name="value={0}, expected={1}")
+    @CsvFileSource(resources = "/test-data.csv")
+    @Order(5)
+    void testWithDataFile(int value, String expected) {
+
+        assertEquals(expected, FizzBuzz.compute(value));
     }
 }
