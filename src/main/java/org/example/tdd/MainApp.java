a5827c1db87d47a2c91994f83958104311191e80
Mariya <russakova.m@gmail.com>
2022-04-27 02:51:15 +0600

    🔢: create MainApp

---
+++ src/main/java/org/example/tdd/MainApp.java
@@ -0,0 +1,12 @@
+package org.example.tdd;
+
+public class MainApp {
+
+    public static void main(String[] args) {
+
+        for (int i=1; i <= 100; i++) {
+
+            System.out.println(FizzBuzz.compute(i));
+        }
+    }
+}
