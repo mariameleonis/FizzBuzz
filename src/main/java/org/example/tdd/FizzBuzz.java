9ed9c67baa02715d8d417223bf55c9e36a3485bf
Mariya <russakova.m@gmail.com>
2022-04-27 02:26:54 +0600

    🔢: implement 'compute' in FizzBuzz.java

---
+++ src/main/java/org/example/tdd/FizzBuzz.java
@@ -0,0 +1,22 @@
+package org.example.tdd;
+
+public class FizzBuzz {
+    public static String compute(int i) {
+
+        StringBuilder result = new StringBuilder();
+
+        if (i % 3 == 0) {
+            result.append("Fizz");
+        }
+
+        if (i % 5 == 0) {
+            result.append("Buzz");
+        }
+
+        if (result.length() == 0) {
+            result.append(i);
+        }
+
+        return result.toString();
+    }
+}
